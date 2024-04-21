894e268ce70bd83e9afc7f8bbcdff03fa2fc9395
Melanie Klein <mk4759a@american.edu>
2024-04-21 11:59:21 -0400

    HW11

--- Homework/Klein_Homwork11_Q3.R
+++ Homework/Klein_Homwork11_Q3.R
@@ -1,52 +1,10 @@
 # Melanie Klein
 # Shiny App HW2
+# Problem 3
 
 library(tidyverse)    
 library(shiny)
 library(shinyWidgets)
-
-
-# Problem 2   
-# Now, continuing adding additional code to the coding chunk for problem 1 to create the Reactive shiny app below.  
-# Note that your app produces three Reactive elements; two plots and a dynamic table.
-
-diamonds%>%
-  select(carat,price,x,y,z) -> diamonds1
-diamonds1
-
-ui <- fluidPage(
-  titlePanel("Frequency Plots, Box Plots, and a Table"),
-  sidebarLayout(      
-    sidebarPanel(
-      selectInput("DVvar", "Diamond Variables:", 
-                  choices=colnames(diamonds1)),
-    ),
-    mainPanel(
-      plotOutput(outputId = "FrequencyPlot"),
-      plotOutput(outputId = "BoxPlot"),
-      dataTableOutput(outputId = "Table")
-    )
-  ))
-
-server <- function(input, output) {
-  output$FrequencyPlot <- renderPlot({
-    ggplot(diamonds, mapping = aes(x = .data[[input$DVvar]], color = cut)) +
-      geom_freqpoly(binwidth = 0.1) +
-      ggtitle("frequency polygons")
-  })
-  
-  output$BoxPlot <- renderPlot({
-    ggplot(diamonds, mapping = aes(x = cut, y = .data[[input$DVvar]])) +
-      geom_boxplot(fill = "purple") +
-      ggtitle("boxplot")
-  })
-  
-  output$Table <- renderDataTable({
-    diamonds1
-  })
-}
-
-shinyApp(ui = ui, server = server)
 
 # Problem 3
 # Now produce your Problem 2 app using tabs as demonstrated in class and use background color or color scheme of your choice (other than white).
@@ -64,7 +22,7 @@ ui <- fluidPage(
   ),
   sidebarLayout(      
     sidebarPanel(
-      selectInput("DVvar", "Diamond Variables:", 
+      selectInput("DVvar", "Diamond Variables", 
                   choices=colnames(diamonds1)),
     ),
     mainPanel(
@@ -72,8 +30,8 @@ ui <- fluidPage(
                   tabPanel("FrequencyPlot", plotOutput("FrequencyPlot")),
                   tabPanel("BoxPlot", plotOutput("BoxPlot")),
                   tabPanel("Table", dataTableOutput("Table"))
-              
-  ))))
+                  
+      ))))
 
 server <- function(input, output) {
   output$FrequencyPlot <- renderPlot({
