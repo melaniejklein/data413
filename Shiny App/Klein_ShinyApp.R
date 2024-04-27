c5825aad19dc697a5dd3b0832e08e59e32a01447
Melanie Klein <mk4759a@american.edu>
2024-04-27 14:29:20 -0400

    shiny app

---
+++ Shiny App/Klein_ShinyApp.R
@@ -0,0 +1,52 @@
+# Melanie Klein
+# Shiny App DATA-413-001
+
+
+# The names of all who contributed to the Shiny app project should be visible at the top of the Shiny App Project as well as a three or four sentence description / introduction of the app. 10 points
+# The Shiny App should have a different and robust background layout (you may use coding and techniques covered in class or from another source)  10 points
+# Three instances of Reactivity 30 points
+# Evidence of the usage of Shiny App coding covered in class 20 points
+# Evidence of the usage of Shiny App coding not covered in class.  15 points
+# The usage of or creation of a shiny app data source that is original and robust. 10 points
+# Overall complexity of the Shiny App. 15 points.
+# Your app should feature attributes that require detailed coding which results in shiny app outcomes that are original and interesting.
+# Shiny App Project Presentation  15 points
+# (Presentation can be be in class, on Zoom, or Recorded)
+
+
+
+
+Ideas
+x
+y
+filter by
+
+
+slider
+user input
+tabs
+show outliers checkbox
+write umber of observations to view
+
+summary stats table
+histogram
+scatterplot
+boxplot
+table
+map
+
+gradient or background color
+different size and center text
+color of points
+
+runExample("01_hello")      # a histogram
+runExample("02_text")       # tables and data frames
+runExample("03_reactivity") # a reactive expression
+runExample("04_mpg")        # global variables
+runExample("05_sliders")    # slider bars
+runExample("06_tabsets")    # tabbed panels
+runExample("07_widgets")    # help text and submit buttons
+runExample("08_html")       # Shiny app built from HTML
+runExample("09_upload")     # file upload wizard
+runExample("10_download")   # file download wizard
+runExample("11_timer")      # an automated timer
